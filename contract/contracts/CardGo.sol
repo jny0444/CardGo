27c6c8331f9a6089e2efcf15b2144956ae56beb7
Jnyandeep <thokchom.jnyandeep@gmail.com>
2024-10-31 19:13:39 +0530

    setup: setup Hardhat Ignition

--- contract/contracts/CardGo.sol
+++ contract/contracts/CardGo.sol
@@ -5,24 +5,6 @@ contract CardGo {
 
     address public owner;
 
-    struct Card {
-        uint256 id;
-        string name;
-        string description;
-        uint256 price;
-        uint256 stock;
-    }
-
-    struct User {
-        address userAddress;
-        uint256 ingameMoney;
-        uint256[] cardIds;
-        Card[] userCards;
-    }
-     
-    Card[] public cards;
-    User[] public users;
-
     constructor() {
         owner = msg.sender;
     }
@@ -35,6 +17,6 @@ contract CardGo {
     // make a modifier that checks user is initialized only once
     
     function initCard() public {  // Add address _addr 
-        users.push(User(msg.sender, 1000, new uint256[](0), new Card[](0)));
+        uint256 card = 0;
     }
 }
